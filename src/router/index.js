6d7e7e19939340461f695eed63025c68ae9a1bcd
Elsa.Syu <elsa.syu@dexatek.com>
2023-01-25 23:46:48 +0800

    update

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import loginStore from '@/store/loginStore.js'
 import Home from '@/views/productView.vue'
 import Login from '@/views/LoginView.vue'
-const history = createWebHistory()
+const history = createWebHistory('/vue_week4')
 const routes = [
   {
     path: '/',
