f0024d481503b1fb6ef52b234d137eee371bdb3b
Elsa.Syu <elsa.syu@dexatek.com>
2023-01-25 21:45:02 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,11 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: '/vue_week4/',
+  base: './',
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src')
-    }
-  }
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
